0c9b82953c8e5ffded453456e4ea4e9aebd1b6cd
Cansu Kavili <cansukavili@gmail.com>
2024-10-23 12:38:01 +0200

    🐈argocd rbac updated🐈

--- install.sh
+++ install.sh
@@ -18,6 +18,9 @@ helm upgrade --install ml500-base . --namespace ml500 --create-namespace
 
 oc patch --type=merge OAuth/cluster -p '{"spec": {"identityProviders": [{"name": "Students", "type": "HTPasswd", "mappingMethod": "claim", "htpasswd": {"fileData": {"name": "htpasswd-ml500"}}}, {"name": "htpasswd_provider", "type": "HTPasswd", "mappingMethod": "claim", "htpasswd": {"fileData": {"name": "htpasswd"}}}]}}'
 
+# Patch Argo CD to be cluster level (for the training purposes)
+
+
 # Configure TrustyAI
 oc patch ConfigMap/user-workload-monitoring-config  -p '{"data": {"config.yaml": "prometheus:\n  logLevel: debug\n  retention: 15d"}}' -n openshift-user-workload-monitoring
 oc patch DataScienceCluster/default-dsc -p '{"spec": {"components": {"trustyai": {"managementState": "Managed", "devFlags": {"manifests": [{"contextDir": "config", "sourcePath": "", "uri": "https://github.com/RHRolun/trustyai-service-operator/tarball/main"}]}}}}}'
